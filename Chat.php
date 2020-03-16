2b2afc8980c41636b1a74730d9e426774218fbfe
ArrowX17 <iamsamix17@gmail.com>
2020-03-17 04:29:05 +0600

    [One-to-One Chat COMPLETED]

--- Chat.php
+++ Chat.php
@@ -1,3 +1,11 @@
+<?php include "Handle/processStudentProfile.php";
+  if(!(isset($_SESSION['StudentEmail']) || isset($_SESSION['FacultyEmail']))) {
+     header('Location: index.php');
+  }
+  if(!(isset($_SESSION))) {
+    include "session.php";
+  }
+?>
 <!DOCTYPE html>
 <html>
 
@@ -11,6 +19,14 @@
     <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
     <link rel="stylesheet" href="assets/css/Bootstrap-Chat.css">
     <link rel="stylesheet" href="assets/css/style.css">
+    <script>
+      function toggleclass(i) {
+        document.getElementById(i).classList.add('active');
+        // document.getElementById("MyElement").classList.remove('MyClass');
+        // if ( document.getElementById("MyElement").classList.contains('MyClass') )
+        // document.getElementById("MyElement").classList.toggle('MyClass');
+      }
+    </script>
 </head>
 
 <body>
@@ -40,99 +56,14 @@
       <div class="bg-white">
 
         <div class="bg-gray px-4 py-2 bg-light">
-          <p class="h5 mb-0 py-1">Recent</p>
+          <p class="h5 mb-0 py-1">Available</p>
         </div>
 
         <div class="messages-box">
           <div class="list-group rounded-0">
-            <a class="list-group-item list-group-item-action active text-white rounded-0">
-              <div class="media"><img src="https://res.cloudinary.com/mhmd/image/upload/v1564960395/avatar_usae7z.svg" alt="user" width="50" class="rounded-circle">
-                <div class="media-body ml-4">
-                  <div class="d-flex align-items-center justify-content-between mb-1">
-                    <h6 class="mb-0">Jason Doe</h6><small class="small font-weight-bold">25 Dec</small>
-                  </div>
-                  <p class="font-italic mb-0 text-small">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore.</p>
-                </div>
-              </div>
-            </a>
-
-            <a href="#" class="list-group-item list-group-item-action list-group-item-light rounded-0">
-              <div class="media"><img src="https://res.cloudinary.com/mhmd/image/upload/v1564960395/avatar_usae7z.svg" alt="user" width="50" class="rounded-circle">
-                <div class="media-body ml-4">
-                  <div class="d-flex align-items-center justify-content-between mb-1">
-                    <h6 class="mb-0">Jason Doe</h6><small class="small font-weight-bold">14 Dec</small>
-                  </div>
-                  <p class="font-italic text-muted mb-0 text-small">Lorem ipsum dolor sit amet, consectetur. incididunt ut labore.</p>
-                </div>
-              </div>
-            </a>
-
-            <a href="#" class="list-group-item list-group-item-action list-group-item-light rounded-0">
-              <div class="media"><img src="https://res.cloudinary.com/mhmd/image/upload/v1564960395/avatar_usae7z.svg" alt="user" width="50" class="rounded-circle">
-                <div class="media-body ml-4">
-                  <div class="d-flex align-items-center justify-content-between mb-1">
-                    <h6 class="mb-0">Jason Doe</h6><small class="small font-weight-bold">9 Nov</small>
-                  </div>
-                  <p class="font-italic text-muted mb-0 text-small">consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore.</p>
-                </div>
-              </div>
-            </a>
-
-            <a href="#" class="list-group-item list-group-item-action list-group-item-light rounded-0">
-              <div class="media"><img src="https://res.cloudinary.com/mhmd/image/upload/v1564960395/avatar_usae7z.svg" alt="user" width="50" class="rounded-circle">
-                <div class="media-body ml-4">
-                  <div class="d-flex align-items-center justify-content-between mb-1">
-                    <h6 class="mb-0">Jason Doe</h6><small class="small font-weight-bold">18 Oct</small>
-                  </div>
-                  <p class="font-italic text-muted mb-0 text-small">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore.</p>
-                </div>
-              </div>
-            </a>
-
-            <a href="#" class="list-group-item list-group-item-action list-group-item-light rounded-0">
-              <div class="media"><img src="https://res.cloudinary.com/mhmd/image/upload/v1564960395/avatar_usae7z.svg" alt="user" width="50" class="rounded-circle">
-                <div class="media-body ml-4">
-                  <div class="d-flex align-items-center justify-content-between mb-1">
-                    <h6 class="mb-0">Jason Doe</h6><small class="small font-weight-bold">17 Oct</small>
-                  </div>
-                  <p class="font-italic text-muted mb-0 text-small">consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore.</p>
-                </div>
-              </div>
-            </a>
-
-            <a href="#" class="list-group-item list-group-item-action list-group-item-light rounded-0">
-              <div class="media"><img src="https://res.cloudinary.com/mhmd/image/upload/v1564960395/avatar_usae7z.svg" alt="user" width="50" class="rounded-circle">
-                <div class="media-body ml-4">
-                  <div class="d-flex align-items-center justify-content-between mb-1">
-                    <h6 class="mb-0">Jason Doe</h6><small class="small font-weight-bold">2 Sep</small>
-                  </div>
-                  <p class="font-italic text-muted mb-0 text-small">Quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                </div>
-              </div>
-            </a>
-
-            <a href="#" class="list-group-item list-group-item-action list-group-item-light rounded-0">
-              <div class="media"><img src="https://res.cloudinary.com/mhmd/image/upload/v1564960395/avatar_usae7z.svg" alt="user" width="50" class="rounded-circle">
-                <div class="media-body ml-4">
-                  <div class="d-flex align-items-center justify-content-between mb-1">
-                    <h6 class="mb-0">Jason Doe</h6><small class="small font-weight-bold">30 Aug</small>
-                  </div>
-                  <p class="font-italic text-muted mb-0 text-small">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore.</p>
-                </div>
-              </div>
-            </a>
-
-            <a href="#" class="list-group-item list-group-item-action list-group-item-light rounded-0">
-              <div class="media"><img src="https://res.cloudinary.com/mhmd/image/upload/v1564960395/avatar_usae7z.svg" alt="user" width="50" class="rounded-circle">
-                <div class="media-body ml-4">
-                  <div class="d-flex align-items-center justify-content-between mb-3">
-                    <h6 class="mb-0">Jason Doe</h6><small class="small font-weight-bold">21 Aug</small>
-                  </div>
-                  <p class="font-italic text-muted mb-0 text-small">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore.</p>
-                </div>
-              </div>
-            </a>
-
+            <?php
+            include "ChatAvailableFaculty.php";
+            ?>
           </div>
         </div>
       </div>
@@ -140,77 +71,81 @@
     <!-- Chat Box-->
     <div class="col-7 px-0">
       <div class="px-4 py-5 chat-box bg-white">
-        <!-- Sender Message-->
-        <div class="media w-50 mb-3"><img src="https://res.cloudinary.com/mhmd/image/upload/v1564960395/avatar_usae7z.svg" alt="user" width="50" class="rounded-circle">
-          <div class="media-body ml-3">
-            <div class="bg-light rounded py-2 px-3 mb-2">
-              <p class="text-small mb-0 text-muted">Test which is a new approach all solutions</p>
-            </div>
-            <p class="small text-muted">12:00 PM | Aug 13</p>
-          </div>
-        </div>
-
-        <!-- Reciever Message-->
-        <div class="media w-50 ml-auto mb-3">
-          <div class="media-body">
-            <div class="bg-primary rounded py-2 px-3 mb-2">
-              <p class="text-small mb-0 text-white">Test which is a new approach to have all solutions</p>
-            </div>
-            <p class="small text-muted">12:00 PM | Aug 13</p>
-          </div>
-        </div>
-
-        <!-- Sender Message-->
-        <div class="media w-50 mb-3"><img src="https://res.cloudinary.com/mhmd/image/upload/v1564960395/avatar_usae7z.svg" alt="user" width="50" class="rounded-circle">
-          <div class="media-body ml-3">
-            <div class="bg-light rounded py-2 px-3 mb-2">
-              <p class="text-small mb-0 text-muted">Test, which is a new approach to have</p>
-            </div>
-            <p class="small text-muted">12:00 PM | Aug 13</p>
-          </div>
-        </div>
-
-        <!-- Reciever Message-->
-        <div class="media w-50 ml-auto mb-3">
-          <div class="media-body">
-            <div class="bg-primary rounded py-2 px-3 mb-2">
-              <p class="text-small mb-0 text-white">Apollo University, Delhi, India Test</p>
-            </div>
-            <p class="small text-muted">12:00 PM | Aug 13</p>
-          </div>
-        </div>
-
-        <!-- Sender Message-->
-        <div class="media w-50 mb-3"><img src="https://res.cloudinary.com/mhmd/image/upload/v1564960395/avatar_usae7z.svg" alt="user" width="50" class="rounded-circle">
-          <div class="media-body ml-3">
-            <div class="bg-light rounded py-2 px-3 mb-2">
-              <p class="text-small mb-0 text-muted">Test, which is a new approach</p>
-            </div>
-            <p class="small text-muted">12:00 PM | Aug 13</p>
-          </div>
-        </div>
-
-        <!-- Reciever Message-->
-        <div class="media w-50 ml-auto mb-3">
-          <div class="media-body">
-            <div class="bg-primary rounded py-2 px-3 mb-2">
-              <p class="text-small mb-0 text-white">Apollo University, Delhi, India Test</p>
-            </div>
-            <p class="small text-muted">12:00 PM | Aug 13</p>
-          </div>
-        </div>
-
-      </div>
+        <?php
+        include "ChatSenderReceiver.php";
+        ?>
 
       <!-- Typing area -->
-      <form action="#" class="bg-light">
+      <form method="post" class="bg-light">
         <div class="input-group">
-          <input type="text" placeholder="Type a message" aria-describedby="button-addon2" class="form-control rounded-0 border-0 py-4 bg-light">
+          <input type="text" placeholder="Type a message" aria-describedby="button-addon2" class="form-control rounded-0 border-0 py-4 bg-light" name="TextField">
           <div class="input-group-append">
-            <button id="button-addon2" type="submit" class="btn btn-link"> <i class="fa fa-paper-plane"></i></button>
+            <button id="button-addon2" type="submit" class="btn btn-link" name="SendMessage"> <i class="fa fa-paper-plane"></i></button>
           </div>
         </div>
       </form>
+      <?php
+      if(isset($_POST['SendMessage'])) {
+        $msg = htmlentities($_POST['TextField']);
+        if($msg == "") {
+          echo "<div class='alert alert-danger'>
+  				        <strong><center>Please enter your message first!</center></strong>
+  				      </div>";
+        } else if(strlen($msg) > 100){
+				echo "<div class='alert alert-danger'>
+				        <strong><center>Message is Too long! Use only 100 characters</center></strong>
+				      </div>";
+			} else {
+        include("Handle/loginDB.php");
+        $formDBLink = mysqli_connect($host, $user, $password, $dbname);
+        if($formDBLink-> connect_error) {
+          die("Connection Failed:".$formDBLink-> connect_error);
+        }
+        if(isset($_GET['FacultyCourses'])) {
+          $formInfoQuery = "INSERT INTO messages(Sender, Receiver, Message)
+          VALUES (
+            '".$StudentCourses."',
+            '".$FacultyCourses."',
+            '".$msg."'
+          )";
+          if($Result = mysqli_query($formDBLink, $formInfoQuery)) {
+            mysqli_close($formDBLink);
+            ?>
+              <script>
+                window.location.href = "chat.php?FacultyCourses=<?php echo $FacultyCourses ?>";
+              </script>
+            <?php
+          } else {
+            echo "<div class='alert alert-danger'>
+                    <strong><center>Error Sending Message!</center></strong>
+                  </div>";
+            mysqli_close($formDBLink);
+          }
+        } else if(isset($_GET['StudentCourses'])) {
+          $formInfoQuery = "INSERT INTO messages(Sender, Receiver, Message)
+          VALUES (
+            '".$FacultyCourses."',
+            '".$StudentCourses."',
+            '".$msg."'
+          )";
+          if($Result = mysqli_query($formDBLink, $formInfoQuery)) {
+            mysqli_close($formDBLink);
+            ?>
+              <script>
+                window.location.href = "chat.php?StudentCourses=<?php echo $StudentCourses ?>";
+              </script>
+            <?php
+          } else {
+            echo "<div class='alert alert-danger'>
+                    <strong><center>Error Sending Message!</center></strong>
+                  </div>";
+            mysqli_close($formDBLink);
+          }
+        }
+      }
+      }
+      ?>
+
 
     </div>
   </div>
